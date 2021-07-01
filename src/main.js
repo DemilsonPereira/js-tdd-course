cc0b2c8cc230def398df83b70f60bbe132dd8419
Demilson Pereira <demilson.d2102@gmail.com>
2021-07-01 11:23:40 -0400

    Feito na prática o uso de test com exemplo básico.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-const a = 3;
-const b = 2;
-
-function sum() {
-  return a + b;
+function sum(num1, num2) {
+  return num1 + num2
 }
 
-sum();
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return (num2 === 0) ? 'Não é possível divisão por zero' : num1 / num2;
+}
+
+module.exports = {
+  sum,
+  sub,
+  mult,
+  div
+}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,37 +1,64 @@
 const expect = require("chai").expect;
+const calc = require('../src/main.js');
 
-describe("Main", function () {
-  let arr;
+describe("Calc", function () {
 
-  // Roda todas as vezes, antes de CADA bloco
-  beforeEach(function () {
-    arr = [1, 2, 3];
+  // smoke test (teste mais básico)
+  describe('Smoke tests', function(){
+
+    it('should exist the calc lib', function(){
+      expect(calc).to.exist;
+    });
+
+    it('should exist the method `sum`', function(){
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a.function;
+    });
+
+    it('should exist the method `sub`', function(){
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a.function;
+    });
+
+    it('should exist the method `mult`', function(){
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a.function;
+    });
+
+    it('should exist the method `div`', function(){
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a.function;
+    });
+
   });
 
-  // testar tipos ou se existe (smoke test)
-  it('should be an array', function(){
-    // Analisando um tipo
-    expect(arr).to.be.a('array');
+  describe('Sum', function(){
+    it('should return 4 when `sum(2,2)`', function(){
+      expect(calc.sum(2,2)).to.be.equal(4);
+    })
   });
 
-  it("should have a size of 4 when push another value to the array", function () {
-    arr.push(4);
-    // Analisando um tamanho
-    expect(arr).to.have.lengthOf(4);
+  describe('Sub', function(){
+    it('should return 6 when `sub(8,2)`', function(){
+      expect(calc.sub(8,2)).to.be.equal(6);
+    })
   });
 
-  it("should remove the value 3 when use pop in the array", function () {
-    arr.pop();
-    // Verificando se contém ou não o valor
-    expect(arr).to.not.include(3);
+  describe('Mult', function(){
+    it('should return 10 when `mult(5,2)`', function(){
+      expect(calc.mult(5,2)).to.be.equal(10);
+    })
   });
 
-  it('should return true when pop 3 from the array', function(){
-    expect(arr.pop() === 3).to.be.true;
+  describe('Div', function(){
+    it('should return 3 when `div(9,3)`', function(){
+      expect(calc.div(9,3)).to.be.equal(3);
+    });
+
+    it('should return `Não é possível divisão por zero` when divide by 0', function(){
+      expect(calc.div(9,0)).to.be.equal('Não é possível divisão por zero');
+    });
+
   });
 
-  it("should have a size of 2 when a value from the array", function () {
-    arr.pop();
-    expect(arr).to.have.lengthOf(2);
-  });
 });
